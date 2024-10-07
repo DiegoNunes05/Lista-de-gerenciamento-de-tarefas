88c60b1f2624ba7efb4b685e4d9b7faa7f8a3a09
Diego Nunes <diegonunesabc@gmail.com>
2024-10-07 01:05:14 -0300

    commit para obrigar o signOut sempre que a pagina for carregada

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, signOut } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -15,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+signOut(auth)
 export const db = getFirestore(app);
