e115a0b39d145054de8a21b714dd1a7c4b8583d5
Diego Nunes <diegonunesabc@gmail.com>
2024-10-06 23:30:51 -0300

    Solucao de direcionamento de pagina

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Lista-de-gerenciamento-de-tarefas/', 
+  base: process.env.NODE_ENV === 'production' ? '/Lista-de-gerenciamento-de-tarefas/' : '/',
   plugins: [vue()],
 })
